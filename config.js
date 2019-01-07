03e26047083778931e2146ba54b6e12de10c9f4a
= <craig@craigshoemaker.net>
2019-01-07 15:53:34 -0500

    Update error message

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const SERVER_LOCATION = 'https://spinefeed.azurewebsites.net'; //'http://localho
 const config = {
     spinefeedUrl: `${SERVER_LOCATION}/api/article`,
     messages: {
-        invalidArticleType: 'Invalid article type. Spinefeed only provides feedback for Quickstarts, Tutorials, and Overview articles. You can change the article type by updating the "ms.author" field in the article metadata.'
+        invalidArticleType: 'Invalid article type. Spinefeed only provides feedback for Quickstarts, Tutorials, and Overview articles.\n\nYou can change the article type by updating the "ms.topic" field in the article metadata.'
     }
 };
 
